217cc621d58b356f01c13585ea7c3d4646595ed1
Felix Barros <felixbarros@chaco.gob.ar>
2021-10-06 16:12:05 -0300

    agragamos la configuracion de api platform para la entidad bicicleta #5

--- src/Entity/Bicycle.php
+++ src/Entity/Bicycle.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\BicycleRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=BicycleRepository::class)
+ * @ApiResource()
  */
 class Bicycle
 {
